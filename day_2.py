e6c63b2af74a0f3ad7f72618a3fffe4b9346de7b
Eman Zadi <eman.zadi58@gmail.com>
2026-07-13 23:54:57 +0500

    Create day_2.py
    Learning variables in python.

---
+++ day_2.py
@@ -0,0 +1,10 @@
+# Learning variables in python.
+
+# Storing text in a variable.
+a="How are you?"  
+print(a)
+b="I hope that you are fine."
+print(b)
+# Storing a number in a variable
+c=5     
+print(c)
