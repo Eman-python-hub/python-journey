859a8e26e708b4598897babdafdf1c9dd38c6740
Eman Zadi <eman.zadi58@gmail.com>
2026-07-14 23:17:51 +0500

    Create day_3.py
    Learning data types in python.

---
+++ day_3.py
@@ -0,0 +1,28 @@
+# Learning data types in python
+
+# There are many types of data , but today i discuss only three data types here:
+# 1-Numeric data type (integer,float,complex)
+# 2-String data type
+# 3-Boolean data type
+ 
+# Numeric data types
+a=123
+print(a)                     # integer
+b=1.2
+print(b)                     # float
+c=2+3j
+print(c)                     # complex
+
+# String data type
+name="Eman Zadi"
+print(name)
+
+# Boolean data type
+e=True
+print(e)
+
+print("The type of a is",type(a))
+print("The type of b is",type(b))
+print("The type of c is",type(c))
+print("The type of name is",type(name))
+print("The type of e is",type(e))
