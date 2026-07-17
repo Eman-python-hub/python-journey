dd15e71c7353b2af45510daf1056095acde802ad
Eman Zadi <eman.zadi58@gmail.com>
2026-07-17 23:06:24 +0500

    Create day_5.py
    Learning collection data types in python.

---
+++ day_5.py
@@ -0,0 +1,19 @@
+               # Collection data types
+
+# List data type (Mutable) 
+
+a=["school","pen","notebook"]
+print("The type of a is:",type(a))
+
+# Tuple data type (Immutable) 
+  
+b=("apple","mango","banana")           
+print("The type of b is:",type(b)) 
+
+# Dict data type (key value)
+
+c={"name": "Eman","age": 16,"section":"A"}        
+print("The type of c is:",type(c))   
+
+
+
