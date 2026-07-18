2854611608c11b6d003299cf692318474670982e
Eman Zadi <eman.zadi58@gmail.com>
2026-07-18 23:58:57 +0500

    Create day_6.py
    Learning operators in python.

---
+++ day_6.py
@@ -0,0 +1,11 @@
+# Learning operators in python
+a=5
+b=8
+print(a+b)    # addition operator
+print(a-b)    # subtraction operator
+print(a*b)    # Multiplication operator
+print(a/b)    # Division operator
+print(a//b)   # Floor division operator
+print(a**b)   # Power operator
+print(a%b)    # Remainder operator
+
