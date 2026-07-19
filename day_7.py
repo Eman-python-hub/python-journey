fa756b63859719b09c882fa91459a98fce4dce81
Eman Zadi <eman.zadi58@gmail.com>
2026-07-19 23:36:08 +0500

    Create day_7.py
    Building a calculator using python.

---
+++ day_7.py
@@ -0,0 +1,12 @@
+# My first project.
+# Building calculator using python.
+a=5
+b=8
+print("The value of",a,"+",b,"is", a+b)
+print("The value of",a,"-",b,"is",a-b)
+print("The value of",a,"*",b,"is",a*b)
+print("The value of",a,"/",b,"is",a/b)
+print("The value of",a,"%",b,"is",a%b)
+print("The value of",a,"//",b,"is",a//b)
+print("The value of",a,"**",b,"is",a**b)
+
