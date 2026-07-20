175f8cefd7573d9984b1dde5af9b8da5e94e2292
Eman Zadi <eman.zadi58@gmail.com>
2026-07-20 23:25:53 +0500

    Create day_8.py
    Learning typecasting in python.

---
+++ day_8.py
@@ -0,0 +1,23 @@
+# Learning typecasting in python
+
+
+# There are two types of typecasting:
+# 1- Implicit typecasting
+# 2- Explicit typecasting
+
+
+# Exploring Explicit typecasting(done by the programmer)
+a="5"
+b="8"
+print(int(a)+int(b))
+
+
+# Exploring Implicit typecasting(done by the python interpreter)
+c=0.5
+d=8
+print(c+d)
+
+
+
+
+
