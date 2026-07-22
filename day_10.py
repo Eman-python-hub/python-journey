1e8938fc5602de086043d32a4445a547baabd0da
Eman Zadi <eman.zadi58@gmail.com>
2026-07-22 23:45:23 +0500

    Create day_10.py
    Learning indexing.

---
+++ day_10.py
@@ -0,0 +1,14 @@
+name="Eman"
+friend="Horan"
+greeting='''Hello Horan,
+How are you?
+I hope that you are fine.'''
+
+# Learning indexing
+
+print(name)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+
